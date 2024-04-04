040025b3f5bd804dcfd3993889d450331da3f23d
mizu1h <166052295+mizu1h@users.noreply.github.com>
2024-04-04 17:29:46 -0300

    Add files via upload

---
+++ TP1_Rodriguez_Alan/src/ar/edu/unju/fi/ejercicio1/main.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n1 = 8, n2 = 5, n3 = 1, n4 = 20, n5 = 9;
+		
+		double promedio = (n1+n2+n3+n4+n5) / 5.0;
+		System.out.println("El promedio es: " + promedio);
+
+	}
+
+}
